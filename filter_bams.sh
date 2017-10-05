812ae1fd8951559d881e2565163ae3187133e1a4
Katie Yost <kdriest@stanford.edu>
2017-10-05 16:35:17 -0700

    Remove filter bams

--- filter_bams.sh
+++
@@ -1,2 +0,0 @@
-# using pre-fix of fastq file 
-python pipelines/filter_bams.py -P 3 -M 100 -O test_out -S BJ_filtered_bams -G hg19 -C filter_bams.yaml -I test_data/BJ_bams/ -I2 test_data/test_peaks.bed    
