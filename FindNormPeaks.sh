1982796712307bac8c77bac1ebb1bdd66b45e532
Katie Yost <kdriest@stanford.edu>
2017-10-11 17:28:23 -0700

    Remove optional arguments from example commands

--- FindNormPeaks.sh
+++ FindNormPeaks.sh
@@ -1,1 +1,1 @@
-python pipelines/FindNormPeaks.py -O APT_practice/ -S NormPeaks -G hg38 -C FindNormPeaks.yaml -I test_data/ENCODE_bams -return 500 -gs hs
+python pipelines/FindNormPeaks.py -O APT_practice/ -S NormPeaks -G hg38 -I test_data/ENCODE_bams -gs hs
--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,1 @@
-python pipelines/ATACPrimerTool.py -O APT_practice -S APT_test -G hg38 -C ATACPrimerTool.yaml -I test_data/ENCODE_bams -B test_data/test_peaks.bed -corr 0.8 -cov 3 -window 100 -overlap 0.9 -counts
+python pipelines/ATACPrimerTool.py -O APT_practice -S APT_test -G hg38 -I test_data/ENCODE_bams -B test_data/test_peaks.bed -counts
