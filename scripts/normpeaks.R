149e53427f86e7dfc1fc9897193c5a1675a924ac
Katie Yost <kdriest@stanford.edu>
2017-10-11 18:00:18 -0700

    Update NormPeaks output

--- scripts/normpeaks.R
+++ scripts/normpeaks.R
@@ -22,14 +22,14 @@ out_file <- args[3]
 Mergedpeak_counts <- read.delim(counts_file, "\t", header = FALSE)
 m <- ncol(Mergedpeak_counts)
 rownames(Mergedpeak_counts) <- Mergedpeak_counts$V4
-countsTable <- Mergedpeak_counts[,7:m]
+countsTable <- Mergedpeak_counts[,5:m]
 n <- ncol(countsTable)
 colData <- data.frame(condition = factor(c(paste(rep("Sample", n),seq(n)))))
 dds <- DESeqDataSetFromMatrix(countsTable,colData, design = ~1)
 dds <-estimateSizeFactors(dds)
-normalizedCounts <- cbind(Mergedpeak_counts[,1:6],data.frame(t(t(counts(dds)) /sizeFactors(dds))))
-normalizedCounts$mean <- rowMeans(normalizedCounts[,7:m])
-normalizedCounts$cv <- apply(normalizedCounts[,7:m], 1, sd)/normalizedCounts$mean
+normalizedCounts <- cbind(Mergedpeak_counts[,1:4],data.frame(t(t(counts(dds)) /sizeFactors(dds))))
+normalizedCounts$mean <- rowMeans(normalizedCounts[,5:m])
+normalizedCounts$cv <- apply(normalizedCounts[,5:m], 1, sd)/normalizedCounts$mean
 normalizedCounts <- normalizedCounts[order(normalizedCounts$cv),]
 normalizedCounts <- normalizedCounts[normalizedCounts$mean > mean(normalizedCounts$mean),]
 normPeaks <- normalizedCounts[1:return_n,]
